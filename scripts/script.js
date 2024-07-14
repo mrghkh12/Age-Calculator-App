0c7d9aad8737b01a069b945cf9036b694fa8db7b
mohammad reza <mrghkh12@gmail.com>
2024-07-14 12:11:45 +0330

    feat(script.js): add event to calculate Btn

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,11 @@
 const $ = document
 
+let calculateBtn = $.querySelector('.calculateBtn')
 let userInput = $.querySelector('#dateInput')
 userInput.max = new Date().toISOString().split('T')[0]
+
+ calculateBtn.addEventListener('click' , calculateDate)
+
+ function calculateDate(){
+
+ }
