fa63a4bf6205f44bebf1e4fed81c2e4e46917c37
mohammad reza <mrghkh12@gmail.com>
2024-07-14 14:05:27 +0330

    style: clean codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,54 @@
-const $ = document
+const $ = document;
 
-let calculateBtn = $.querySelector('.calculateBtn')
-let userInput = $.querySelector('#dateInput')
-let calculateResult = $.querySelector('#calculateResult')
-userInput.max = new Date().toISOString().split('T')[0]
+let calculateBtn = $.querySelector(".calculateBtn");
+let userInput = $.querySelector("#dateInput");
+let calculateResult = $.querySelector("#calculateResult");
+userInput.max = new Date().toISOString().split("T")[0];
 
- calculateBtn.addEventListener('click' , calculateDate)
+calculateBtn.addEventListener("click", calculateDate);
 
- function calculateDate(){
-    if(userInput.value !== ''){
-        let userInputDate = new Date(userInput.value)
+function calculateDate() {
+  if (userInput.value !== "") {
+    let userInputDate = new Date(userInput.value);
 
-        let userInputDay = userInputDate.getDate()
-        let userInputMonth = userInputDate.getMonth() + 1
-        let userInputYear = userInputDate.getFullYear()
-    
-        let nowDate = new Date()
-    
-        let nowDay = nowDate.getDate()
-        let nowMonth = nowDate.getMonth() + 1
-        let nowYear = nowDate.getFullYear()
-    
-        let resultDay , resultMonth , resultYear;
-    
-        resultYear = nowYear - userInputYear
-    
-        if(nowMonth >= userInputMonth){
-            resultMonth = nowMonth - userInputMonth
-        }else{
-            resultYear--;
-            resultMonth = 12 + nowMonth - userInputMonth
-        }
-    
-        if(nowDay >= userInputDay){
-            resultDay = nowDay - userInputDay
-        }else{
-            resultMonth--;
-            resultDay = grtDaysInMonth(userInputYear,userInputMonth) + nowDay - userInputDay
-        }
-        
-        if(resultMonth < 0){
-            resultMonth = 11
-            resultYear--
-        }
-    
-        calculateResult.innerHTML = `You are <span>${resultYear}</span> years, <span>${resultMonth}</span> months and <span>${resultDay}</span> days old`
-    
+    let userInputDay = userInputDate.getDate();
+    let userInputMonth = userInputDate.getMonth() + 1;
+    let userInputYear = userInputDate.getFullYear();
+
+    let nowDate = new Date();
+
+    let nowDay = nowDate.getDate();
+    let nowMonth = nowDate.getMonth() + 1;
+    let nowYear = nowDate.getFullYear();
+
+    let resultDay, resultMonth, resultYear;
+
+    resultYear = nowYear - userInputYear;
+
+    if (nowMonth >= userInputMonth) {
+      resultMonth = nowMonth - userInputMonth;
+    } else {
+      resultYear--;
+      resultMonth = 12 + nowMonth - userInputMonth;
     }
- }
 
- function grtDaysInMonth(year,month){
-    return new Date(year,month,0).getDate()
- }
+    if (nowDay >= userInputDay) {
+      resultDay = nowDay - userInputDay;
+    } else {
+      resultMonth--;
+      resultDay =
+        grtDaysInMonth(userInputYear, userInputMonth) + nowDay - userInputDay;
+    }
+
+    if (resultMonth < 0) {
+      resultMonth = 11;
+      resultYear--;
+    }
+
+    calculateResult.innerHTML = `You are <span>${resultYear}</span> years, <span>${resultMonth}</span> months and <span>${resultDay}</span> days old`;
+  }
+}
+
+function grtDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
