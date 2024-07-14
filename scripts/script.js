eb25c408c8692c151b0c611d73ec3873a95eed2f
mohammad reza <mrghkh12@gmail.com>
2024-07-14 12:43:05 +0330

    feat(script.js): Create date function calculate to middle

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,32 @@ userInput.max = new Date().toISOString().split('T')[0]
  calculateBtn.addEventListener('click' , calculateDate)
 
  function calculateDate(){
+    let userInputDate = new Date(userInput.value)
 
+    let userInputDay = userInputDate.getDate()
+    let userInputMonth = userInputDate.getMonth() + 1
+    let userInputYear = userInputDate.getFullYear()
+
+    let nowDate = new Date()
+
+    let nowDay = nowDate.getDate()
+    let nowMonth = nowDate.getMonth() + 1
+    let nowYear = nowDate.getFullYear()
+
+    let resultDay , resultMonth , resultYear;
+
+    resultYear = nowYear - userInputYear
+
+    if(nowMonth >= userInputMonth){
+        resultMonth = nowMonth - userInputMonth
+    }else{
+        resultYear--;
+        resultMonth = 12 + nowMonth - userInputMonth
+    }
+
+    if(nowDay >= userInputDay){
+        resultDay = nowDay - userInputDay
+    }else{
+        
+    }
  }
